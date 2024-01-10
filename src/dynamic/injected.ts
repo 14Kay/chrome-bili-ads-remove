2ca517ac06192a0d333f88a29d78eda8958672b2
14kk <619113277@qq.com>
2024-01-10 18:56:29 +0800

    ✨ feat: 过滤b站自家的广告平台的广告

--- src/dynamic/injected.ts
+++ src/dynamic/injected.ts
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: 14K
  * @Date: 2023-11-13 20:35:50
- * @LastEditTime: 2023-11-13 20:53:08
+ * @LastEditTime: 2024-01-10 18:44:23
  * @LastEditors: 14K
  */
 let keywords: string[] = []
@@ -22,11 +22,23 @@ window.fetch = async function (url: any, options) {
                     const dynamicLst = dynamic.data.items
                     dynamic.data.items = dynamicLst.filter((item: any) => {
                         if(item.type == "DYNAMIC_TYPE_DRAW" || item.type == "DYNAMIC_TYPE_WORD"){
-                            const content = item.modules.module_dynamic.major.opus.summary.text
-                            return !keywords.some((item) => content.includes(item))
+                            if(item.modules.module_dynamic.desc){
+                                const content = item.modules.module_dynamic.desc.text || ""
+                                return !keywords.some((item) => content.includes(item))
+                            }
+
+                            if(item.modules.module_dynamic.additional){
+                                return false
+                            }
+                            
+                            if(item.modules.module_dynamic.major && item.modules.module_dynamic.major.opus){
+                                const content = item.modules.module_dynamic.major.opus.summary.text || ""
+                                return !keywords.some((item) => content.includes(item))
+                            }
                         }
                         return true
                     })
+                    
                     // 创建包含修改后内容的新响应对象
                     const modifiedResponse = new Response(JSON.stringify(dynamic), {
                         status: response.status,
